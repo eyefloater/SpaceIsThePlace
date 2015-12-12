32e1a54abd3147d8ec0c0b4c11b47a4dc85a00ee
eyefloater <gobifish@gmail.com>
2015-12-11 22:45:36 -0600

    ...

--- src/com/gaby/space/GameDataBase.java
+++ src/com/gaby/space/GameDataBase.java
@@ -69,9 +69,19 @@ public class GameDataBase {
 			//database of 'rooms'. 
 			stmt.execute("CREATE TABLE ROOMS (ID INT, NAME VARCHAR(20), DESCRIPTION VARCHAR(8000))");
 			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (1, 'Inventory', '')");
-			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (2, 'Lunar Surface', 'The sun has just dipped out of sight. The smooth surface of the lunar plain extends to the horizon. \nGOO parks his 2-track just off the road, climbs out with a spare tank and looks back up the road as he shuts the door. \nTo the North, the Medtronic enclave with its vast dome of age-warped, slightly murky plastic. \n\nGOO turns, feeling a little weak in the legs as he scrambles down the embankment to stride \nout into the silent, glimmering lunar countryside.')");
-			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (3, 'Boardwalk','The boardwalk that runs along the Zone's northern boundary extends before GOO, its stained plastic planks lit with overlapping planes of garish neon reds, blues, and sickly greens that emanate from the dense jumble of tourist shops that loom over its landward side. To the west, , behind the flickering, 100 year old projected ocean view, GOO can hear the grinding and hum of the platinum mines. GOO's family property is a single 4-story tower of corrugated tin and plastic wedged between a brothel and a PayDay Loan shop. Outside his grandfather's ground floor cafe, a dozen folded Fanta Limon parasols emerge haphazardly from a jumble of plastic tables, flapping lamely in the artificial ocean-scented breeze. Inside, the cafe is brightly lit but deserted; GOO's friend Agnes is washing glasses. Two brothel workers puff on E-cigarettes at the mouth of the narrow alley, wrapped in long, silvery miner's jackets, clearly at the end of their shift.')");
-			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (4, 'Space Bar','THE SPACIOUS SPACE BAR, as the neon sign proclaims is in fact quite a spacious place. It is completely deserted, except for Agnes, who is washing an endless series of glasses in the sink behind the counter. The stereo is playing the usual mixture of 20th century ye-ye, country, and modern Algorhythmia pop. What dross.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (2, 'Lunar Surface SOUTH', 'About 100 clicks south of the Zone, GOO comes across an abandoned rover.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (3, 'Lunar Surface', 'The sun has just dipped out of sight. The smooth surface of the lunar plain extends to the horizon. \nGOO parks his 2-track just off the road, climbs out with a spare tank and looks back up the road as he shuts the door. \nTo the North, the Medtronic enclave with its vast dome of age-warped, slightly murky plastic. \n\nGOO turns, feeling a little weak in the legs as he scrambles down the embankment to stride \nout into the silent, glimmering lunar countryside.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (4, 'Boardwalk','The boardwalk that runs along the Zone's northern boundary extends before GOO, its stained plastic planks lit with overlapping planes of garish neon reds, blues, and sickly greens that emanate from the dense jumble of tourist shops that loom over its landward side. To the west, , behind the flickering, 100 year old projected ocean view, GOO can hear the grinding and hum of the platinum mines. GOO's family business, the Space Bar Bar, is a one story affair of corrugated tin and , wedged between a brothel and a PayDay Loan shop, both boarded up. A dozen folded Fanta Limon parasols emerge haphazardly from a jumble of plastic tables outside the bar, flapping lamely in the artificial ocean-scented breeze. To the NORTH, the cafe is brightly lit but deserted; GOO's friend Nico is washing glasses. To the WEST, a miner mans the Medtronic checkpoint, puffing an E-cigarete To the EAST, GOO can see the holographic marquee of the Jupiter Club.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (5, 'Space Bar','THE SPACE BAR BAR is indeed quite spacious inside. It is also completely deserted, except for Nico, who stands washing an endless series of glasses in the sink behind the counter. The stereo is playing the usual mixture of 20th century ye-ye, country, and modern Algorhythmia pop. GOO sees his wallet ontop of the gaming console.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (6, 'Back room', 'This is were GOO sleeps. There's a small cot, a space heater, Goo's record player and records, and a large pile of dirty clothes. Home sweet home. ')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (7, 'Boardwalk EAST', 'The eastern stretch of the Medtronic boardwalk is bustling with what passes for nightlife on 2270's Luna. The Jupiter club, with it's holographic marquee, is to the NORTH, Guitar Center is to the EAST.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (8, 'Jupiter Club', 'The Jupiter Club is less busy than it used to be, when mining was still a viable career choice in this particular company town. Still, the house band, a motley group of Martian refugees called the Tireds From Mars is churning out some passable covers of 2050's asteroid mining ballads. The manager, Louis, watches them with a glazed look on his face. ')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (9, 'Rehearsal Space', 'The nearly empty rehearsal room; at least there's a working PA.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (10, 'Shop', 'Guitar Center. GOO has seen all this before. The shopkeeper looks slightly sinister. GOO notices a gleaming silver drumkit in the corner.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (11, 'Alley', 'GOO steps into the alley behind the shop. The dumpsters are overflowing. There's a large box in the shadows; a thin reedy buzz emanates from within. ')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (12, 'Mining HQ', 'The noise around the mining facility is unbearable. There's no pleasant projected ocean view here, just a monstrous accumulation of ore, and at least a dozen 40 year old robots shoveling away methodically at the pile. The single human around is your old boss, Hugo F. He's up to his shins in dust, still cursing in his wild Venusian accent to some unlucky Medtronic Customer Service AI over the phone, brandishing an actual shovel in his other hand.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (13, 'Old refinery', 'A broad concrete expanse bounded by the grim outbuildings of an oil refinery. The surface of the walls are so featureless nearby that GOO is afraid they're computer generated, but they darken with rich grime as they recede into the complex. Aggie sees him before he sees her. She is already running northwards, one hand dragging along the east wall as she skips backwards. She knows he will follow, as he always does- hence the mixture of joy and irony on her face as it recedes into the narrowing half shadow. Beneath the grime, the shadowy walls behind her start to glow a rosy pink, as if in response to her presence; as if he were entering her heart.')");
+			stmt.execute("INSERT INTO ROOMS (ID, NAME, DESCRIPTION) VALUES (14, 'Spaceship', 'GOO loves all spaceships. But this one is particularly rad. Very sleek, in the classic featureless-silver-cube style of the 2240's. A clearly intoxicated spaceship captain is sitting near the coffee machine, muttering to himself')");
 			
 			//probably get rid of this. openDoor can just be a method that is called to check if you have a key, when 
 			//you enter N S E or W and there is a door in the way. 
@@ -87,9 +97,22 @@ public class GameDataBase {
 			stmt.execute("CREATE TABLE OBJECTS (ID INT, LOCATION INT, NAME VARCHAR(12), DESCRIPTION VARCHAR(64), OWNED BOOLEAN)");
 			stmt.execute("INSERT INTO OBJECTS (ID, LOCATION, NAME, DESCRIPTION, OWNED) VALUES (1, 1, 'tank', 'Goo likes breathing', FALSE)");
 			stmt.execute("INSERT INTO OBJECTS (ID, LOCATION, NAME, DESCRIPTION, OWNED) VALUES (1, 2, 'moon rock', 'Shiny moon rock', FALSE)");
+			stmt.execute("INSERT INTO OBJECTS (ID, LOCATION, NAME, DESCRIPTION, OWNED) VALUES (1, 2, 'rover', 'This little guy should be easy to carry.', FALSE)");
 			stmt.execute("INSERT INTO OBJECTS (ID, LOCATION, NAME, DESCRIPTION, OWNED) VALUES (2, 2, 'Sun Ra's saxophone', 'The real deal.', FALSE)");
 			stmt.execute("INSERT INTO OBJECTS (ID, LOCATION, NAME, DESCRIPTION, OWNED) VALUES (3, 4, 'CUP', 'The cup says I LOVE NY. GOO has no idea what ny means or how it's pronounced.', FALSE)");
-			
+			//rover
+			//wallet
+			//ID
+			//AGGIE's CREDIT STICK
+			//LEATHER JACKET
+			//RECORDS
+			//RECORD PLAYER
+			//DRUMKIT
+			//LANYARDS
+			//ROBOTS
+			//ORE
+			//LARGE BOX
+			//
 			//commiting sends it from client to the database, checks for errors
 			connection.commit();
 			stmt.close();
