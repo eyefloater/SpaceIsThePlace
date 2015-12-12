4e4791781af936a1844aa4465737d21343f0b7e3
eyefloater <gobifish@gmail.com>
2015-12-12 13:13:31 -0600

    .

---
+++ src/com/gaby/space/Dialog.java
@@ -0,0 +1,55 @@
+package com.gaby.space;
+
+//use database, i guess. 
+
+//Nico: where's my ID? (wallet was empty.), will you join the band?(he says yes)
+
+//Charlie (guy at checkpoint): do you have ID? what's your band's name? (if not Nico's suggestion, wont let you through.
+
+//Aggie: make peace with old friend, ask if will join (yes, but not if band named by Nico. suggests name. also, needs DRUMS first)
+
+//Jupiter Club manager, Louis: got band and gear? ok you can rehearse. listens after rehearsal. hires if you give Aggie's bandname. gives you laminated employee LANYARDS for cruise ship
+
+//guy w saxophone. longwinded story of Sun Ra. gives you SAX.
+
+//shop guy. sells you DRUMS if you have enuf money. suggests you "get a damn job, miner"
+
+//mining foreman, won't hire you permanently cos you fucked it up last time, why are you even on this damn rock if you hate it so much , btw i'll pay you to recover ROVER
+
+//cruise ship guy. no tree here, just welcome, get on stage, blast off...
+
+public class Dialog {
+
+	public static void Nico() {
+
+	}
+
+	public static void Aggie() {
+
+	}
+
+	public static void Charlie() {
+
+	}
+
+	public static void saxGuy() {
+
+	}
+
+	public static void Louis() {
+
+	}
+
+	public static void miningForeman() {
+
+	}
+
+	public static void shopKeeper() {
+
+	}
+
+	public static void shipCaptain() {
+
+	}
+
+}
